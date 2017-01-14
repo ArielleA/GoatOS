dd7c5ca00023ef875ee4ca4cafd0b55ce245b1bc
Arielle Albon <arielle@hopeless-newbie.co.uk>
2017-01-14 23:30:14 +0100

    Last changes over to new class structure. Cleanup pending.

--- tools/dtbread.py
+++ tools/dtbread.py
@@ -81,6 +81,109 @@ class FDTStrings:
             else:
                 raise IndexError
 
+class FDTNode:
+    def __init__(self,name):
+        self.name = name
+        self.children = []
+        self.properties = {}
+   
+    def add_child(self,location):
+        self.children.append(location)
+
+    def get_children(self):
+        return self.children
+
+    def add_property(self, name, value=None):
+        if b'#' in name:
+            value = struct.unpack('!I',value)[0]
+        elif name in [b'phandle']:
+            value = struct.unpack('!I',value)[0]
+        elif name == b'compatible':
+            value = value.strip(b'\x00').decode('utf-8')
+            value = value.split('\x00')
+        elif name in [b'device_type',b'method',b'bootargs',b'model']:
+            value = value.strip(b'\x00').decode('utf-8')
+        self.properties[name.decode('utf-8')]=value
+
+    def get_properties(self):
+        return self.properties
+    
+    def __getitem__(self,key):
+        return self.properties[key]
+
+    def keys(self):
+        return self.properties.keys()
+
+    def get_name(self):
+        return self.name
+
+class FDTStruct:
+    def __init__(self, string, length, properties):
+        self.string = string
+        self.length = length
+        self.properties = properties
+        self.nodes = {}
+        self.root = 0
+
+    def get_command(self,offset):
+        cmd = struct.unpack_from('!I',self.string,offset=offset)[0]
+        #if cmd not in [1,2,3,4,9]:
+        #    raise ValueError('Command not recognised')
+        offset=offset+struct.calcsize('!I')
+        return (cmd,offset)
+
+    def get_root_node(self):
+        return self.root   
+
+    def __getitem__(self, key):
+        return self.nodes[key]
+
+    def new_node(self,location, offset):
+        #First we get the name of the node
+        nameidx = self.string [ offset : ].find(b'\0')
+        name = self.string [ offset : offset + nameidx ]
+        string_offset=offset+calc_length_word_align(nameidx+1)
+        node = FDTNode(name)
+        self.nodes[location] = node
+        return string_offset 
+
+    def new_property(self,curnode,offset,properties):
+        property_len, property_nameoff = struct.unpack_from('!II',self.string, offset=offset)
+        offset=offset+struct.calcsize('!II')
+        property_name = properties[property_nameoff]
+        property_val = self.string[offset:offset+property_len]
+        offset=offset+calc_length_word_align(property_len)
+        self.nodes[curnode].add_property(property_name,property_val)
+        return offset
+
+    def process_struct(self):
+        offset = 0
+        curnode = 0
+        stack = []
+        while offset < self.length:
+            cmd,next_offset = self.get_command(offset)
+            #Process Commands
+            if cmd == 1:
+                #Check if we are root, if not append on stack
+                if offset != 0:
+                   stack.append(curnode)
+                #Create new node and return next offset
+                next_offset = self.new_node(offset,next_offset)
+                #If we are the root node, do not add link to self
+                if offset != 0:
+                    self.nodes[curnode].add_child(offset)
+                #Make ourself be the current node
+                curnode = offset
+            elif cmd == 2:
+                if stack != []: 
+                    curnode = stack.pop()
+            elif cmd == 3:
+                next_offset = self.new_property(curnode,next_offset,self.properties)
+            elif cmd == 9:
+                break;
+            offset = next_offset
+
+
 def read_memory_reservations(string,offset):
     reservations = []
     while 1:
@@ -137,41 +240,64 @@ def read_struct_block(string,offset,length,properties):
             print (curnode_num)
         elif cmd == 4:
             print ("Found CMD",cmd,"which is a NOP, ignoring")
+        elif cmd == 9:
+            print ("Found CMD",cmd,"which is the end of struct block")
+            return nodes
         else:
             print("Unknown Command",cmd)  
             print (nodes)
             break
         
+def debug_info_header(header):
+   print (colored("Header:",'cyan'),colored("Valid FDT magic value found",'green',attrs=['bold']))
+   print (colored("Header",'cyan'),"-> Total Size of file:     ",colored('{0:>8d} {0:>#8x}'.format(header.totalsize),'yellow'))
+   print (colored("Header",'cyan'),"-> Offset to Struct Block: ",colored('{0:>8d} {0:>#8x}'.format(header.off_dt_struct),'yellow')," with size: ",colored('{0:>8d} {0:>#8x}'.format(header.size_dt_struct),'yellow'))
+   print (colored("Header",'cyan'),"-> Offset to String Block: ",colored('{0:>8d} {0:>#8x}'.format(header.off_dt_strings),'yellow')," with size: ",colored('{0:>8d} {0:>#8x}'.format(header.size_dt_strings),'yellow'))
+   print (colored("Header",'cyan'),"-> Offset to Memory Reser: ",colored('{0:>8d} {0:>#8x}'.format(header.off_mem_rsvmap),'yellow'))
+   print (colored("Header",'cyan'),"-> Version of DTB:         ",colored('{0:>8d} {0:>#8x}'.format(header.version),'yellow'))
+   print (colored("Header",'cyan'),"-> Previous Version of DTB:",colored('{0:>8d} {0:>#8x}'.format(header.last_comp_version),'yellow'))
+   print (colored("Header",'cyan'),"-> Boot CPU Number:        ",colored('{0:>8d} {0:>#8x}'.format(header.boot_cpuid_phys),'yellow'))
+   print
 
+def debug_node(fdt,node,depth,path):
+   depth=depth+1
+   path=path+fdt[node].get_name()+b'/'
+   print (colored("Node:",'cyan'),"-> ",colored(path.decode('ascii'),'green'),'{')
+   for each in fdt[node].keys():
+       print (colored("Node:",'cyan'),"-> ","   "*depth,each,"=",colored(fdt[node][each],'yellow'))
+   for each in fdt[node].get_children():
+       debug_node(fdt,each,depth,path)
+   print (colored("Node:",'cyan'),"-> ","   "*depth,"};")
 
+def debug_info_struct(fdt):
+   #Traverse node tree in depth first
+   depth = 0
+   path = b''
+   root=fdt.get_root_node()
+   debug_node(fdt,root,depth,path)
+   
 if __name__ == '__main__':
    a = open('xenvm-4.2.dtb','rb').read()
    header = FDTHeader()
    header.process_from_string(a)
 
    #print "Raw Header Tuple",header
-   print ("Hex of Magic Value:",colored(format(header.magic,'x'),'blue'))
+   #print ("Hex of Magic Value:",colored(format(header.magic,'x'),'blue'))
    if header.magic == 0xd00dfeed:
-        print (colored("Header:",'cyan'),colored("Valid DTB magic value found",'green',attrs=['bold']))
-        print (colored("Header",'cyan'),"-> Total Size of file:     ",colored('{0:>8d} {0:>#8x}'.format(header.totalsize),'yellow'))
-        print (colored("Header",'cyan'),"-> Offset to Struct Block: ",colored('{0:>8d} {0:>#8x}'.format(header.off_dt_struct),'yellow')," with size: ",colored('{0:>8d} {0:>#8x}'.format(header.size_dt_struct),'yellow'))
-        print (colored("Header",'cyan'),"-> Offset to String Block: ",colored('{0:>8d} {0:>#8x}'.format(header.off_dt_strings),'yellow')," with size: ",colored('{0:>8d} {0:>#8x}'.format(header.size_dt_strings),'yellow'))
-        print (colored("Header",'cyan'),"-> Offset to Memory Reser: ",colored('{0:>8d} {0:>#8x}'.format(header.off_mem_rsvmap),'yellow'))
-        print (colored("Header",'cyan'),"-> Version of DTB:         ",colored('{0:>8d} {0:>#8x}'.format(header.version),'yellow'))
-        print (colored("Header",'cyan'),"-> Previous Version of DTB:",colored('{0:>8d} {0:>#8x}'.format(header.last_comp_version),'yellow'))
-        print (colored("Header",'cyan'),"-> Boot CPU Number:        ",colored('{0:>8d} {0:>#8x}'.format(header.boot_cpuid_phys),'yellow'))
-        print
-        b = read_memory_reservations(a,header.off_mem_rsvmap)
-        if b == []:
-            print (colored("Reservations",'cyan'),'-> There are no memory reservations')
-        else:
-           for each in b:
-               print (colored("Reservations",'cyan'),'-> Memory Reservation at: ',colored('{0:>#8x}'.format(each[0]))," for size: ",colored('{0:>#8x}'.format(each[1])))
-        #c = read_string_block(a,header.off_dt_strings,header.size_dt_strings)
-        c = FDTStrings()
-        c.set_raw_string(a[header.off_dt_strings:header.off_dt_strings+header.size_dt_strings],header.size_dt_strings)
-        print (c)
-        d = read_struct_block(a,header.off_dt_struct,header.size_dt_struct,c)
+      debug_info_header(header)
+      b = read_memory_reservations(a,header.off_mem_rsvmap)
+      if b == []:
+          print (colored("Reservations",'cyan'),'-> There are no memory reservations')
+      else:
+          for each in b:
+              print (colored("Reservations",'cyan'),'-> Memory Reservation at: ',colored('{0:>#8x}'.format(each[0]))," for size: ",colored('{0:>#8x}'.format(each[1])))
+      #c = read_string_block(a,header.off_dt_strings,header.size_dt_strings)
+      c = FDTStrings()
+      c.set_raw_string(a[header.off_dt_strings:header.off_dt_strings+header.size_dt_strings],header.size_dt_strings)
+      d = FDTStruct(a[header.off_dt_struct:],header.size_dt_struct,c)
+      d.process_struct()
+      debug_info_struct(d)
+      #d = read_struct_block(a,header.off_dt_struct,header.size_dt_struct,c)
    else:
         print (coloured("Magic value not found. Aborted!"),'red',attrs=['bold'])
         sys.exit(1)
